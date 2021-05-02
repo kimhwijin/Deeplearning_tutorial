187d8db461596343a8f46bc177b99c06f1ff0912
kimhwijin <azxsdc0160@naver.com>
2021-05-02 21:03:19 +0900

    activation function and MNINST dataset predict implement

---
+++ MNINST.py
@@ -0,0 +1,77 @@
+import sys,os
+from MNINST_dataset.mnist import load_mnist
+import numpy as np
+from PIL import Image
+import pickle
+from activation_function import sigmoid_func,softmax_func
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+def init_MNINST():
+    #load dataset
+    (x_train,t_train),(x_test,t_test) = load_mnist(flatten=True,normalize=False)
+    
+    #test
+    img = x_train[0]
+    label = t_train[0]
+    print(label)
+    print(img.shape)
+    img = img.reshape(28,28)
+    print(img.shape)
+    img_show(img)
+
+def get_data():
+    (x_train,t_train),(x_test,t_test) = load_mnist(flatten=True,normalize=True,one_hot_label=False)
+    return x_test,t_test
+
+def init_network():
+    with open("MNINST_dataset/sample_weight.pkl",'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network,x):
+    W1,W2,W3 = network['W1'], network['W2'], network['W3']
+    b1,b2,b3 = network['b1'], network['b2'], network['b3']
+    
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid_func(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid_func(a2)
+    a3 = np.dot(z2,W3) + b3
+    y = softmax_func(a3)
+
+    return y
+
+
+def test_predict():
+    x,t = get_data()
+    network = init_network()
+
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network,x[i])
+        p = np.argmax(y)
+        if p == t[i]:
+            accuracy_cnt += 1
+    print("Accuracy :" + str(float(accuracy_cnt / len(x))))
+
+def test_with_batch_predict():
+    x,t = get_data()
+    network = init_network()
+
+    batch_size = 0
+    accuracy_cnt = 0
+
+    for i in range(0,len(x),batch_size):
+        x_batch = x[i:i+batch_size]
+        y_batch = predict(network,x_batch)
+        p = np.argmax(y_batch,axis=1)
+        accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+    print("Accuracy :" + str(float(accuracy_cnt / len(x))))
+
+if __name__ == "__main__":
+    test_predict()  
--- activation_function.py
+++ activation_function.py
@@ -3,9 +3,8 @@
 y = h(b + x1w1 + x2w2)
 h(x) : 0 (x <= 0) , 1 (x > 0)
 '''
-import math
 def sigmoid_func(x):
-    hx = 1/(1 + math.exp(-x))
+    hx = 1/(1 + np.exp(-x))
     return hx
 
 import numpy as np
@@ -14,8 +13,54 @@ def step_func(x):
     y = x > 0
     return y.astype(np.int)
 
+def relu_func(x):
+    return np.maximum(0,x)
+
+def identify_func(x):
+    return x
+
+def softmax_func(a):
+    max_a = np.max(a)
+    exp_a = np.exp(a - max_a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1,0.2,0.3])
+    network['W2'] = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+    network['b2'] = np.array([0.1,0.2])
+    network['W3'] = np.array([[0.1,0.3],[0.2,0.4]])
+    network['b3'] = np.array([0.1,0.2])
+    return network
+
+def multi_layer_sigmoid(network, X):
+    W1 = network['W1']
+    B1 = network['b1']
+    A1 = np.dot(X,W1) + B1
+    Z1 = sigmoid_func(A1)
+
+    W2 = network['W2']
+    B2 = network['b2']
+    A2 = np.dot(Z1,W2) + B2
+    Z2 = sigmoid_func(A2)
+
+    W3 = network['W3']
+    B3 = network['b3']
+    A3 = np.dot(Z2,W3) + B3
+    Y = A3
+    return Y
+
+network = init_network()
+x = np.array([1.0,0.5])
+y = multi_layer_sigmoid(network,x)
+print(y)
+
+
 x = np.arange(-5.0,5.0,0.1)
-y = step_func(x)
+y = sigmoid_func(x)
 plt.plot(x,y)
 plt.ylim(-0.1,1.1)
 plt.show()
