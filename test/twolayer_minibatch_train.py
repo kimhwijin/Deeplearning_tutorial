f3fc0a7e25ab3f4be2fcb6ca5a54a731ba72f9d1
kimhwijin <azxsdc0160@naver.com>
2021-05-04 20:04:28 +0900

    add epoch accuracy

--- test/twolayer_minibatch_train.py
+++ test/twolayer_minibatch_train.py
@@ -5,14 +5,19 @@ from MNINST_dataset.mnist import load_mnist
 import numpy as np
 
 (x_train, t_train), (x_test,t_test) = load_mnist(normalize=True,one_hot_label=True)
+network = TwoLayerNet(input_size=784,hidden_size=50,output_size=10)
 
-train_loss_list = []
-
+#hyper parameter
 iters_num = 2000
-train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
-network = TwoLayerNet(input_size=784,hidden_size=50,output_size=10)
+train_size = x_train.shape[0]
+
+train_loss_list = []
+train_accuracy_list = []
+test_accuracy_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size,batch_size)
@@ -25,5 +30,17 @@ for i in range(iters_num):
     for key in ('W1','b1','W2','b2'):
         network.params[key] -= learning_rate * grad[key]
     
+    #record iter loss
     loss = network.loss(x_batch,t_batch)
     train_loss_list.append(loss)
+
+    #accuracy per epoch 
+    if i % iter_per_epoch == 0 and i != 0:
+        train_accuracy = network.accuracy(x_train,t_train)
+        test_accuracy = network.accuracy(x_test, t_test)
+        train_accuracy_list.append(train_accuracy)
+        test_accuracy_list.append(test_accuracy)
+        print("train acc, test acc | " + str(train_accuracy) + ", " + str(test_accuracy))
+
+
+
